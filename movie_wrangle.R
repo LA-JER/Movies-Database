65063fc427275142cad1b491bf22764e2d2158b1
taku-mh <mhuru.tm@gmail.com>
2023-10-11 13:50:44 -0400

    data wrangling file

---
+++ movie_wrangle.R
@@ -0,0 +1,11 @@
+# Load packages
+library(ggplot2)
+library(mdsr)
+library(tidyverse)
+library(Stat2Data)
+library(dplyr)
+library(lubridate)
+library(janitor)
+library(readr)
+
+cast <- readr::read_tsv()
